a1edb73416bdc804b83346dff0f8c08c1612f42e
meriemaya <banounemeriem.mb@gmail.com>
2020-11-24 14:29:06 +0100

    fisrt commit wear app

---
+++ app/src/main/java/com/e/wearappdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.e.wearappdemo;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class MainActivity extends WearableActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextView = (TextView) findViewById(R.id.text);
+
+        // Enables Always-on
+        setAmbientEnabled();
+
+    }
+
+}
